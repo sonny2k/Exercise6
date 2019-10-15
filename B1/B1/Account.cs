15a9660b3eedc21dd8c4db02e5ca0870062f2915
trung <trungtran060720@gmail.com>
2019-10-15 15:27:07 +0700

    b1

---
+++ B1/B1/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B1
+{
+    public class Account
+    {
+        //constructor trung ten voi class
+        public Account(int amount)
+        {
+            int money = amount;
+        }
+
+        public void Deposit(int amount)
+        {
+            amount++;
+        }
+
+        public void Withdraw(int amount)
+        {
+            amount--;
+        }
+
+        public int Balance(int money)
+        {
+            return money;
+        }
+    }
+}
---
+++ B1/TestProblem1/UnitTestAccount.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using B1;
+
+namespace TestProblem1
+{
+    [TestClass]
+    public class UnitTestAccount
+    {
+        [TestMethod]
+        public void TestMethodAccount()
+        {
+            Account acc = new Account(200000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(200000, acc.Balance(money));
+        }
+
+         [TestMethod]
+         public void TestMethodDeposit()
+        {
+            Account acc = new Account(500000);
+
+            Assert.IsNotNull(acc);
+            Assert.IsInstanceOfType(acc, typeof(Account));
+            Assert.AreEqual(700000, acc.Balance(money));
+        }
+
+        [TestMethod]
+         public void TestMethodWithdraw()
+         {
+             Account acc = new Account(300000);
+
+             Assert.IsNotNull(acc);
+             Assert.IsInstanceOfType(acc, typeof(Account));
+             Assert.AreEqual(400000, acc.Balance(money));
+         }
+    }
+}
