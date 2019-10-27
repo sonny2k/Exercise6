dd6a513e78ca4994153310eb25158e9df0bf7726
sonny2k <thanhdepzaipro@gmail.com>
2019-10-27 12:07:09 +0700

    Finsished

---
+++ B1/B2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace B2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack MyStack = new Stack();
+            Console.WriteLine("Add 1st number: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            MyStack.Push(n1);
+            Console.WriteLine("Add 2nd number: ");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            MyStack.Push(n2);
+            Console.WriteLine("Add 3rd number: ");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            MyStack.Push(n3);
+            Console.WriteLine();
+            Console.WriteLine(MyStack.Pop());
+            Console.WriteLine();
+            Console.WriteLine(MyStack.Pop());
+            Console.WriteLine();
+            Console.WriteLine(MyStack.Count);
+        }
+    }
+}
---
+++ B3/Queue.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B3
+{
+    public class Queue
+    {
+        private int[] Buffer;
+        // first position
+        private int f;
+        // last position
+        private int r;
+        // count
+        private int c;
+
+        public Queue(int size)
+        {
+            Buffer = new int[size];
+            f = 0;
+            r = -1;
+            c = 0;
+        }
+
+        public void Enqueue(int item)
+        {
+            Buffer[++r] = item;
+            c++;
+        }
+
+        public int Dequeue()
+        {
+            int x = Buffer[f];
+            for (int i = f; i < r; i++) //move item to the next position 
+                Buffer[i] = Buffer[i + 1];
+            r--; // reduce last position value
+            c--;// reduce quantity of items
+            return x; // return item
+        }
+
+        public int PeekQueue()
+        {
+            int a = Buffer[f];
+            for (int i = f; i < r; i++) //move item to the next position
+                Buffer[i] = Buffer[i + 1];
+            return a;
+        }
+
+        public int Count()
+        {
+            return c;
+        }
+
+        public void Clear()
+        {
+            f = 0;
+            r = -1;
+            c = 0;
+        }
+    }
+}
